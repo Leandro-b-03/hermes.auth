01c83bd5f022f1730a23368bf9afa717b59eb68d
Leandro Bezerra <leandro.b.03@gmail.com>
2024-11-29 18:55:23 -0300

    Update Shipper.php

--- app/Models/Shipper.php
+++ app/Models/Shipper.php
@@ -11,6 +11,35 @@ class Shipper extends Model
     use HasFactory;
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'tax_id',
+        'name',
+        'fantasy_name',
+        'address',
+        'address_2',
+        'address_3',
+        'number',
+        'city',
+        'state',
+        'country',
+        'zip_code',
+        'contact_name',
+        'contact_email',
+        'contact_title',
+        'contact_department',
+        'contact_phone',
+        'contact_mobile',
+        'contact_fax',
+        'contact_document',
+        'logo_image_url',
+        'shipper_matrix_id'
+    ];
+
+    /**
      * Get the users for the shipper.
      */
     public function users(): HasMany
