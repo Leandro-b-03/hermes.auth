773026d0ce637e7aaf95d5622312be938cf6ebb7
Leandro Bezerra <leandro.b.03@gmail.com>
2024-12-03 21:27:08 -0300

    Add logo image to s3

--- app/Http/Controllers/Api/V1/ShipperController.php
+++ app/Http/Controllers/Api/V1/ShipperController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Api\BaseController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ShipperController extends BaseController
 {
@@ -67,6 +69,12 @@ class ShipperController extends BaseController
      */
     public function update(Request $request, $id)
     {
+        $validator = $this->validate($request->all());
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
         $user = auth()->user();
 
         if (!$user->hasRole('admin')) {
@@ -83,15 +91,28 @@ class ShipperController extends BaseController
             return $this->sendError('No data to update.');
         }
 
+        if ($shipper->tax_id !== $request->input('shipper')['tax_id']) {
+            return $this->sendError('Tax ID cannot be updated.');
+        }
+
         try {
             DB::beginTransaction();
-            
+
+            $logo = $request->file('shipper')['logo'];
+
             $values = $request->input('shipper');
 
             foreach ($values as $key => $value) {
                 if ($value === 'null') {
                     $values[$key] = null;
                 }
+            }
+
+            if ($logo) {
+                $s3Disk = Storage::disk('s3');
+
+                $file = $s3Disk->putFileAs('logos', $logo, str_replace('/', '_', $shipper->tax_id) . "_" . str_replace(' ', '_', $logo->getClientOriginalName()));
+                $values['logo_image_url'] = $s3Disk->url($file);                
             }
 
             $updated = $shipper->update($values);
@@ -111,5 +132,41 @@ class ShipperController extends BaseController
             logger($e);
             return $this->sendError('Shipper could not be updated.');
         }
+    }
+
+    /**
+     * Get the tax ID of the shipper.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    protected function validate($request)
+    {
+        $validator = Validator::make($request, [
+            'shipper' => 'required|array',
+            'shipper.name' => 'required|string',
+            'shipper.tax_id' => 'required|string',
+            'shipper.commercial_name' => 'required|string',
+            'shipper.address' => 'required|string',
+            'shipper.address_2' => 'nullable|string',
+            'shipper.address_3' => 'required|string',
+            'shipper.number' => 'required|string',
+            'shipper.city' => 'required|string',
+            'shipper.state' => 'required|string',
+            'shipper.country' => 'required|string',
+            'shipper.zip_code' => 'required|string',
+            'shipper.contact_name' => 'required|string',
+            'shipper.contact_email' => 'required|email',
+            'shipper.contact_title' => 'nullable|string',
+            'shipper.contact_department' => 'nullable|string',
+            'shipper.contact_phone' => 'nullable|string',
+            'shipper.contact_mobile' => 'nullable|string',
+            'shipper.contact_fax' => 'nullable|string',
+            'shipper.contact_document' => 'nullable|string',
+            'shipper.logo_image_url' => 'nullable|string',
+            'shipper.shipper_matrix_id' => 'nullable',
+        ]);
+
+        return $validator;
     }
 }
